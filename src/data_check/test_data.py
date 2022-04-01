3cb41cb137b0bea8587e4a56bda0870d230b328a
Ramin Anushiravani <raminanushiravani@Ramins-MBP.attlocal.net>
2022-03-31 22:18:06 -0700

    adding version 1 of the course project

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -63,3 +63,14 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 ########################################################
 # Implement here test_row_count and test_price_range   #
 ########################################################
+def test_row_count(data: pd.DataFrame):
+    """
+    Checking the number of samples in the dataset
+    """
+    assert 15000 < data.shape[0] < 1000000
+
+def test_price_range(data: pd.DataFrame, min_price: float, max_price: float):
+    """
+    checking the price is within min and max
+    """
+    assert np.sum(data['price'].between(min_price,max_price))
